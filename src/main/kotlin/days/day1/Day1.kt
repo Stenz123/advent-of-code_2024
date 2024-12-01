7049b215664b0188ca7fc4f84b807ec88fbc29b9
mStenz <michael.stenz2006@gmail.com>
2024-12-01 14:28:46 +0100

    Day 1

--- src/main/kotlin/days/day1/Day1.kt
+++ src/main/kotlin/days/day1/Day1.kt
@@ -1,14 +1,28 @@
 package days.day1
 
 import days.Day
+import java.util.PriorityQueue
+import kotlin.math.absoluteValue
 
 class Day1: Day(true) {
     override fun partOne(): Any {
-        return "day 1 part 1 not Implemented"
+        val input = readInput()
+        val (l1, l2) = input.map { line ->
+            val split = line.split(" ").filter(String::isNotBlank).map(String::toLong)
+            split[0] to split[1]
+        }.unzip()
+        return l1.sorted().zip(l2.sorted()){a, b -> (a-b).absoluteValue}.sum()
     }
 
     override fun partTwo(): Any {
-        return "day 1 part 2 not Implemented"
+        val input = readInput()
+        val (l1, l2) = input.map { line ->
+            val split = line.split(" ").filter(String::isNotBlank).map(String::toLong)
+            split[0] to split[1]
+        }.unzip()
+        return l1.sumOf { item ->
+            item * l2.count { it == item }
+        }
     }
 }
 

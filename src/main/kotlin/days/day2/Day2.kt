b36e1b2027b794b18c296cb624006e7ab9e69156
mStenz <michael.stenz2006@gmail.com>
2024-12-02 06:23:06 +0100

    Day 2 p2

--- src/main/kotlin/days/day2/Day2.kt
+++ src/main/kotlin/days/day2/Day2.kt
@@ -3,33 +3,44 @@ package days.day2
 import days.Day
 import kotlin.math.absoluteValue
 
-class Day2: Day(true) {
+class Day2: Day(false) {
     override fun partOne(): Any {
 
         return readInput().count{line ->
-
             val nums = line.split(" ").map (String::toInt)
-
-
-
-            if (!nums.equals(nums.sorted()) && !nums.equals(nums.sortedDescending())) {
+            if (nums != nums.sorted() && nums != nums.sortedDescending()) {
                 return@count false
             }
-
             for (i in 0 until nums.size -1) {
                 if ((nums[i] - nums[i + 1]).absoluteValue !in 1..3) {
                     return@count false
                 }
             }
-            println(nums)
             return@count true
         }
 
     }
 
     override fun partTwo(): Any {
-        return "day 2 part 2 not Implemented"
+        return readInput().count{line ->
+            val nums = line.split(" ").map (String::toInt)
+            loop1@for (index in nums.indices) {
+                val newNums = nums.toMutableList()
+                newNums.removeAt(index)
 
+                if (newNums != newNums.sorted() && newNums != newNums.sortedDescending()) {
+                    continue
+                }
+                for (i in 0 until newNums.size -1) {
+                    println(newNums[i]- newNums[i + 1])
+                    if ((newNums[i] - newNums[i + 1]).absoluteValue !in 1..3) {
+                        continue@loop1
+                    }
+                }
+                return@count true
+            }
+            return@count false
+        }
     }
 }
 

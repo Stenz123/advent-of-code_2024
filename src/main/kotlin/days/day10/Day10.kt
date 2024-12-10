643d6271637c15fc332e093881646cca1ab8d1f1
mStenz <michael.stenz2006@gmail.com>
2024-12-10 08:36:19 +0100

    Day 10 p1

---
+++ src/main/kotlin/days/day10/Day10.kt
@@ -0,0 +1,46 @@
+package days.day10
+
+import days.Day
+
+class Day10: Day(false) {
+    override fun partOne(): Any {
+        val map = hashMapOf<Pair<Int, Int>, Int>()
+        readInput().forEachIndexed{i, line ->
+            line.forEachIndexed{ j, char ->
+                if (char != '.') {
+                    map[i to j] = char.digitToInt()
+                }
+            }
+        }
+
+        fun Pair<Int, Int>.getNeighbours(): List<Pair<Int, Int>> {
+            return listOf(
+                this.first - 1 to this.second,
+                this.first to this.second - 1,
+                this.first to this.second + 1,
+                this.first + 1 to this.second,
+            ).filter { map.containsKey(it) }
+        }
+
+        val reached = mutableSetOf<Pair<Int, Int>>()
+        fun Pair<Int, Int>.hike() {
+            if (map[this] == 9){
+                reached.add(this)
+                return
+            }
+            val neighbours = this.getNeighbours().filter { map[it] == map[this]!!+1 }
+            neighbours.forEach { it.hike() }
+        }
+
+        return map.filter { it.value == 0 }.keys.sumOf{
+            reached.clear()
+            it.hike()
+            reached.size
+        }
+    }
+
+    override fun partTwo(): Any {
+        return "day 10 part 2 not Implemented"
+    }
+}
+

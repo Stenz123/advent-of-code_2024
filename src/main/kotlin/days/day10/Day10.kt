82a9196a98e8ac9f4399d6b354a09176a75e71b6
mStenz <michael.stenz2006@gmail.com>
2024-12-11 09:01:12 +0100

    Day 10 p2

--- src/main/kotlin/days/day10/Day10.kt
+++ src/main/kotlin/days/day10/Day10.kt
@@ -40,7 +40,33 @@ class Day10: Day(false) {
     }
 
     override fun partTwo(): Any {
-        return "day 10 part 2 not Implemented"
+        val map = hashMapOf<Pair<Int, Int>, Int>()
+        readInput().forEachIndexed{i, line ->
+            line.forEachIndexed{ j, char ->
+                if (char != '.') {
+                    map[i to j] = char.digitToInt()
+                }
+            }
+        }
+
+        fun Pair<Int, Int>.getNeighbours(): List<Pair<Int, Int>> {
+            return listOf(
+                this.first - 1 to this.second,
+                this.first to this.second - 1,
+                this.first to this.second + 1,
+                this.first + 1 to this.second,
+            ).filter { map.containsKey(it) }
+        }
+
+        fun Pair<Int, Int>.hike():Int {
+            if (map[this] == 9)return 1
+            val neighbours = this.getNeighbours().filter { map[it] == map[this]!!+1 }
+            return neighbours.sumOf { it.hike() }
+        }
+
+        return map.filter { it.value == 0 }.keys.sumOf{
+            it.hike()
+        }
     }
 }
 
